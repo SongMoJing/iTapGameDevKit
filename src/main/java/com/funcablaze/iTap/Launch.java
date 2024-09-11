0a461b80a3c42311b3942fab8b3b14e3c123baa6
SongMojing <Song_Mojing@outlook.com>
2024-09-11 19:00:56 +0800

    正常更改

---
+++ src/main/java/com/funcablaze/iTap/Launch.java
@@ -0,0 +1,7 @@
+package com.funcablaze.iTap;
+
+public class Launch {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/funcablaze/iTap/Window/Window.java
@@ -0,0 +1,4 @@
+package com.funcablaze.iTap.Window;
+
+public class Window {
+}
